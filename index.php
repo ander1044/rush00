029aa91694d2c95b1abed7a2d0e4d0b9481f85c4
Thabang <tchewa@student.wethinkcode.co.za>
2019-10-15 09:25:52 +0200

    index layout and pictures

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="main.css" />
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <nav>
+                <div class="row">
+                    <div class="menu">
+                        <div class="logo">
+                            <p><span class="icon">FG</span>Fitness Guru</p>
+                        </div>
+                        <ul>
+                            <li>Home</li>
+                            <li>Shop</li>
+                            <!--<li>Services
+                                        <ul>
+                                            <li>Cloud Services</li>
+                                            <li>Domain Registation</li>
+                                            <li>Graphics and Development</li>
+                                        </ul>-->
+                            </li>
+                            <li>Contact Us</li>
+                            <li class="sign-up">Sign Up</li>
+                            <li class="sign-in">Sign In</li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <section class="jumbotron">
+            <div class="container">
+                <div class="row">
+                    <h1 class="jumbotron-heading">Comfort Running.</h1>
+                </div>
+            </div>
+        </section>
+
+        <section class="intro">
+            <div class="container">
+                <div class="row">
+                    <h1 class="header-trends">-Trending this week-</h1>
+                </div>
+            </div>
+        </section>
+
+        <section class="home-categories">
+            <div class="container">
+                <div class="row">
+                    <h1 class="header-cate">-Categories-</h1>
+                </div>
+            </div>
+        </section>
+
+        <section class="trends">
+            <div class="container">
+                <div class="row">
+                    Trending
+                </div>
+            </div>
+        </section>
+
+    </main>
+</body>
+
+</html>
